ef04bf03163aaf4e4de10be075aedadbd62dd077
Seunghoon Ko <imskojs@gmail.com>
2015-05-26 18:42:35 +0900

    (safe) start fitInfo

--- www/states/fitInfo/fitInfo.js
+++ www/states/fitInfo/fitInfo.js
@@ -2,7 +2,7 @@ angular.module('myFitMate')
 .controller('fitInfo', function(
   $scope, $state, Data, Utility
 ){
-
+  // Start
   $scope.categories = Data.fitInfo.categories;
   $scope.subHeaderMenuScroller = Utility.subHeaderMenuScroller;
 
